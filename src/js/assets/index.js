aefea6f237e68d0afc0f54d08b5f44747fe65d9c
fillika <49833052+fillika@users.noreply.github.com>
2019-11-23 18:56:27 +0300

    Почти закончил третий блок

--- src/js/assets/index.js
+++ src/js/assets/index.js
@@ -2,7 +2,19 @@ import "popper.js";
 
 $(document).ready(function() {
   $(".third-block__slider").slick({
-    prevArrow: "<div class='third-block__prevArrow'> <img src='../../img/arr-left.png'></div>",
-    nextArrow: "<div class='third-block__nextArrow'> <img src='../../img/arr-right.png'></div>",
+    prevArrow:
+      "<div class='third-block__prevArrow'> <img src='../../img/arr-left.png'></div>",
+    nextArrow:
+      "<div class='third-block__nextArrow'> <img src='../../img/arr-right.png'></div>",
+    infinite: true,
+    speed: 500,
+    fade: true,
+    cssEase: "linear",
+    asNavFor: '.slider-wrapper__adresses'
+  });
+
+  $(".slider-wrapper__adresses").slick({
+    arrows: false,
+    asNavFor: '.third-block__slider'
   });
 });
