70502d85dd6e44c78d729362602a565b69b55649
fillika <49833052+fillika@users.noreply.github.com>
2019-11-23 18:31:07 +0300

    Блок 3. Продолжаю кастомизацию слайдера

--- src/js/assets/index.js
+++ src/js/assets/index.js
@@ -1,1 +1,8 @@
 import "popper.js";
+
+$(document).ready(function() {
+  $(".third-block__slider").slick({
+    prevArrow: "<div class='third-block__prevArrow'> <img src='../../img/arr-left.png'></div>",
+    nextArrow: "<div class='third-block__nextArrow'> <img src='../../img/arr-right.png'></div>",
+  });
+});
--- src/js/bundle.js
+++ src/js/bundle.js
@@ -1,6 +1,7 @@
 const path = require("path");
 
 /* JS files */
+import "./assets/slick.min";
 import "./assets/index";
 
 /* Styles */
@@ -13,3 +14,5 @@ import img from "../img/slider_1.jpg";
 import img1 from "../img/slider_2.jpg";
 import img2 from "../img/slider_3.jpg";
 import img3 from "../img/slider_4.jpg";
+import img4 from "../img/arr-left.png";
+import img5 from "../img/arr-right.png";
