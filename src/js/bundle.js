ae020cf68e5336fccbeeabd319b03b9c45a0c7e7
fillika <49833052+fillika@users.noreply.github.com>
2019-11-23 18:10:15 +0300

    Начал блок 3. Планирую настраивать slick slider

--- src/js/bundle.js
+++ src/js/bundle.js
@@ -9,4 +9,7 @@ import "../scss/index.scss";
 
 /* img import */
 
-// import img from "../img/smoke.png";
+import img from "../img/slider_1.jpg";
+import img1 from "../img/slider_2.jpg";
+import img2 from "../img/slider_3.jpg";
+import img3 from "../img/slider_4.jpg";
