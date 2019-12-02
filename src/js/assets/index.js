692bf29c044a41d7f6820459a3625f16dba605bd
Aleksandr Mironenko <alfie.mir@gmail.com>
2019-12-02 22:00:03 +0300

    Еще немного настройка слайдера

--- src/js/assets/index.js
+++ src/js/assets/index.js
@@ -1,27 +1,38 @@
 import "popper.js";
 
-$(document).ready(function() {
-  $(".third-block__slider").slick({
-    prevArrow:
-      "<div class='third-block__prevArrow'> <img src='../../img/arr-left.png'></div>",
-    nextArrow:
-      "<div class='third-block__nextArrow'> <img src='../../img/arr-right.png'></div>",
-    infinite: true,
-    speed: 500,
-    fade: true,
-    cssEase: "linear",
-    asNavFor: ".slider-wrapper__adresses"
-  });
+$(document).ready(function () {
+	$(".third-block__slider").slick({
+		prevArrow:
+			"<div class='third-block__prevArrow'> <img src='../../img/arr-left.png'></div>",
+		nextArrow:
+			"<div class='third-block__nextArrow'> <img src='../../img/arr-right.png'></div>",
+		infinite: true,
+		speed: 500,
+		fade: true,
+		cssEase: "linear",
+		asNavFor: ".slider-wrapper__adresses"
+	});
 
-  $(".slider-wrapper__adresses").slick({
-    arrows: false,
-    asNavFor: ".third-block__slider"
-  });
+	$(".slider-wrapper__adresses").slick({
+		arrows: false,
+		asNavFor: ".third-block__slider"
+	});
 
-  const swiper = new Swiper('.swiper-container', {
-    spaceBetween: 20,
-    slidesPerView: 'auto',
-    loop: true,
-    centeredSlides: true
-  })
+	const swiper = new Swiper('.swiper-container', {
+		spaceBetween: 20,
+		slidesPerView: 'auto',
+		loop: true,
+		centeredSlides: true,
+    preloadImages: false,
+    lazy: true,
+		autoplay: {
+			delay: 2000
+		},
+		on: {
+			slideChange: function () {
+				console.log("Slide change")
+			}
+		}
+
+	})
 });
